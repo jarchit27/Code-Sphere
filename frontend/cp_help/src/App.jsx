bef78e88c33efc60a827562cd52db989e22fd829
jarchit27 <j.archit134@gmail.com>
2025-06-29 21:19:45 +0530

    done

--- frontend/cp_help/src/App.jsx
+++ frontend/cp_help/src/App.jsx
@@ -2,22 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
-import { Navigate } from "react-router-dom";
 import ViewAnalysis from "./pages/ViewAnalysis/ViewAnalysis";
 import Problems from "./pages/Problems/Problems";
 import Profile from "./pages/Profile/Profile";
 import Compare from './pages/Compare/Compare';
 import Contests from './pages/Contests/Contests';
-
 import CompareResult from './pages/Compare/CompareResult';
 import Modal from 'react-modal';
+
 Modal.setAppElement('#root');
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/' element={<Login />} /> {/* default route shows login */}
         <Route path='/dashboard' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
